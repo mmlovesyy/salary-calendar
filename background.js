518a2881d1fc3e3f4802eedaffbe52f6483cb399
linus.chen <1061888975@qq.com>
2024-03-24 20:26:03 +0800

    init project

---
+++ background.js
@@ -0,0 +1,19 @@
+/**
+ * Plays audio files from extension service workers
+ * @param {string} source - path of the audio file
+ * @param {number} volume - volume of the playback
+ */
+async function playSound(source = 'printer-25474.mp3', volume = 100) {
+    await createOffscreen();
+    await chrome.runtime.sendMessage({ play: { source, volume } });
+}
+
+// Create the offscreen document if it doesn't already exist
+async function createOffscreen() {
+    if (await chrome.offscreen.hasDocument()) return;
+    await chrome.offscreen.createDocument({
+        url: 'offscreen.html',
+        reasons: ['AUDIO_PLAYBACK'],
+        justification: 'testing' // details for using the API
+    });
+}
